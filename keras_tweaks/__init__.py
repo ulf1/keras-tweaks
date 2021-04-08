bb0ee6e55cc9c439a87143c770ee1f121da03969
UH <554c46@gmail.com>
2021-04-08 12:34:37 +0200

    pkg version variable added

--- keras_tweaks/__init__.py
+++ keras_tweaks/__init__.py
@@ -1,3 +1,5 @@
+__version__ = '0.2.1'
+
 from .idseqs_to_mask import idseqs_to_mask
 from .dense_sparse_matmul import dense_sparse_matmul
 from .get_sparsity_pattern import get_sparsity_pattern
--- setup.py
+++ setup.py
@@ -2,8 +2,18 @@ from setuptools import setup
 import pypandoc
 
 
+def get_version(path):
+    with open(path, "r") as fp:
+        lines = fp.read()
+    for line in lines.split("\n"):
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
+
+
 setup(name='keras-tweaks',
-      version='0.2.0',
+      version=get_version("keras_tweaks/__init__.py"),
       description='Utility functions for Keras/Tensorflow2.',
       long_description=pypandoc.convert('README.md', 'rst'),
       url='http://github.com/ulf1/keras-tweaks',
