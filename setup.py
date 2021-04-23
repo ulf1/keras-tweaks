6968daef4e85bad31b653184621355135f2563b8
UH <554c46@gmail.com>
2021-04-23 21:45:53 +0200

    setuptools removed, zip safe deactivated

--- setup.py
+++ setup.py
@@ -19,11 +19,10 @@ setup(name='keras-tweaks',
       url='http://github.com/ulf1/keras-tweaks',
       author='Ulf Hamster',
       author_email='554c46@gmail.com',
-      license='MIT',
+      license='Apache License 2.0',
       packages=['keras_tweaks'],
       install_requires=[
-          'setuptools>=40.0.0',
           'tensorflow==2.*',
           'sparsity-pattern>=0.4.*'],
       python_requires='>=3.6',
-      zip_safe=True)
+      zip_safe=False)
